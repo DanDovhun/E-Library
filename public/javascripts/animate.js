6075411e904f5384f31ba76e7f1b2b94f4180df9
DannyDov <39409234+DannyMeister01@users.noreply.github.com>
2020-11-15 22:55:11 +0100

    Initialized and started working on the site

---
+++ public/javascripts/animate.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".search-button").mouseenter(function() {
+        $(this).css("background-color", "rgb(160, 210, 160)");
+    });
+
+    $(".search-button").mouseleave(function() {
+        $(this).css("background-color", "rgb(180, 230, 180)");
+    });
+
+    $(".form-control").mouseenter(function() {
+        $(this).css("border", "2px solid rgba(100, 120, 255, 0.8)");
+    });
+
+    $(".form-control").mouseleave(function() {
+        $(this).css("border", "1px solid rgba(100, 120, 255, 0.6)");
+    });
+});
