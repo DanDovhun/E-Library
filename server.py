0c47eaacf0790e6c737a9afaf3b57c10dec10000
DannyDov <39409234+DannyMeister01@users.noreply.github.com>
2021-07-28 15:13:03 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- server.py
+++ server.py
@@ -1,10 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import pymongo as pmg
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html", search=False)
+
+@app.route("/", methods=["POST"])
+def search():
+    myclient = pmg.MongoClient("mongodb://localhost:27017")
+    mydb = myclient["library"]
+    mycol = mydb["books"]
+
+    options = request.form.getlist("filter")
+    search = request.form["search"]
+
+    lst = []
+    count = 0
+
+    print(options)
+    
+    for i in mycol.find():
+        if options == "all":
+            print(i)
+            count += 1
+            lst.append(i)
+
+        if options == "title":
+            if search.lower() in i["title"].lower():
+                count += 1
+                lst.append(i)
+
+        if options == "author":
+            if search.lower() in i["author"].lower():
+                count += 1
+                lst.append(i)
+
+        if options == "language":
+            if search.lower() in i["languages"].lower():
+                count += 1
+                lst.append(i)
+
+        if options == "genre":
+            if search.lower() in i["genre"].lower():
+                count += 1
+                lst.append(i)
+
+        if options == "room":
+            if search.lower() in i["room"].lower():
+                count += 1
+                lst.append(i)
+
+        if options == "shelf":
+            if search.lower() in i["shelf"].lower():
+                count += 1
+                lst.append(i)
+
+    print(lst)
+
+    if count == 0:
+        return render_template("index.html", search=True, found=False)
+    
+    else:
+        return render_template("index.html", search=True, found=True, books=lst)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port="8080")
---
+++ static/js/animate.js
@@ -0,0 +1,143 @@
+$(document).ready(function() {
+    $(".back").click(function() {
+        window.location.replace("/add");
+    });
+
+    $(".form-control").mouseenter(function() {
+        $(this).css("border", "2px solid rgba(100, 120, 255, 0.8)");
+    });
+
+    $(".form-control").mouseleave(function() {
+        $(this).css("border", "1px solid rgba(100, 120, 255, 0.6)");
+    });
+
+    $(".genres").click(function() {
+        $(".all_genres").slideDown(500, function() {
+
+        });
+    });
+
+    $(".hide").click(function() {
+        $(".all_genres").slideUp(750, function() {
+
+        });
+    });
+
+    $(".educational").click(function() {
+        document.getElementById("input").value = "Náučná";
+    });
+
+    $(".adventure").click(function() {
+        document.getElementById("input").value = "Dobrodružná";
+    });
+
+    $(".detective").click(function() {
+        document.getElementById("input").value = "Detektívka";
+    });
+
+    $(".religious").click(function() {
+        document.getElementById("input").value = "Náboženská";
+    });
+
+    $(".travel").click(function() {
+        document.getElementById("input").value = "Cestovateľská";
+    });
+
+    $(".music").click(function() {
+        document.getElementById("input").value = "Hudba";
+    });
+
+    $(".fantasy").click(function() {
+        document.getElementById("input").value = "Fantasy";
+    });
+
+    $(".scifi").click(function() {
+        document.getElementById("input").value = "Sci-fi";
+    });
+
+    $(".kids_stories").click(function() {
+        document.getElementById("input").value = "Detské rozprávky";
+    });
+
+    $(".drama").click(function() {
+        document.getElementById("input").value = "Dráma";
+    });
+
+    $(".dystopia").click(function() {
+        document.getElementById("input").value = "Dystopia";
+    });
+
+    $(".biography").click(function() {
+        document.getElementById("input").value = "Biografia";
+    });
+
+    $(".law").click(function() {
+        document.getElementById("input").value = "Zákon";
+    });
+
+    $(".kids_poems").click(function() {
+        document.getElementById("input").value = "Detské básničky";
+    });
+
+    $(".short_stories").click(function() {
+        document.getElementById("input").value = "Poviedky";
+    });
+
+    $(".dictionary").click(function() {
+        document.getElementById("input").value = "Slovník";
+    });
+
+    $(".health").click(function() {
+        document.getElementById("input").value = "Zdravie";
+    });
+
+    $(".philosophy").click(function() {
+        document.getElementById("input").value = "Filozofia";
+    });
+
+    $(".instructions").click(function() {
+        document.getElementById("input").value = "Návod";
+    });
+
+    $(".diary").click(function() {
+        document.getElementById("input").value = "Denník";
+    });
+
+    $(".photography").click(function() {
+        document.getElementById("input").value = "Fotografia";
+    });
+
+    $(".personal_growth").click(function() {
+        document.getElementById("input").value = "Osobný rast";
+    });
+
+    $(".business").click(function() {
+        document.getElementById("input").value = "Business";
+    });
+
+    $(".politics").click(function() {
+        document.getElementById("input").value = "Politics";
+    });
+
+    $(".life_story").click(function() {
+        document.getElementById("input").value = "Životopis";
+    });
+
+    $(".golf").click(function() {
+        document.getElementById("input").value = "Golf";
+    });
+
+    $(".economics").click(function() {
+        document.getElementById("input").value = "Ekonomika";
+    });
+
+    $(".hospitality").click(function() {
+        document.getElementById("input").value = "Pohostinnosť";
+    });
+
+    $(".close").click(function() {
+        $(".alert").fadeOut(200);
+    });
+    
+    $(".all_genres").hide();
+});
